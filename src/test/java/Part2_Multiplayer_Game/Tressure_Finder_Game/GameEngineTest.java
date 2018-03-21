507dc15821c4a66d2e9ba98dedafcb99fdb3100a
Dylan Galea <dillu_galea@hotmail.com>
2018-03-21 22:56:08 +0100

    added java docs to game engine test

--- src/test/java/Part2_Multiplayer_Game/Tressure_Finder_Game/GameEngineTest.java
+++ src/test/java/Part2_Multiplayer_Game/Tressure_Finder_Game/GameEngineTest.java
@@ -10,17 +10,34 @@ import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
-public class GameEngineTest {
-    private GameEngine treasureGame ;
+/**
+ * This class is used to test the game Engine class , note that only methods that could be tested by junit without
+ * user live input could be tested , that's why the code coverage may be lower than previous tests
+ */
 
+public class GameEngineTest {
+    private GameEngine treasureGame ; //stores the treasure game to be tested
+
+    /**
+     * A rule done in order to test for exceptions
+     */
     @Rule
     public ExpectedException exceptionExcepted = ExpectedException.none();
 
+    /**
+     * This method is used to initialize the treasure game
+     */
     @Before
     public void setUp(){
         treasureGame = new GameEngine();
     }
 
+    /**
+     * This method tests whether the validation of the player input is per assignment specification
+     * @throws InvalidNumberOfPlayersException
+     * When the test fails this throws an InvalidNumberOfPlayersException since the inputted number would be out
+     * of range
+     */
     @Test
     public void testValidNumberOfPlayersInput() throws InvalidNumberOfPlayersException{
         assertEquals(true,treasureGame.validNumberOfPlayers(2));
@@ -28,12 +45,23 @@ public class GameEngineTest {
         assertEquals(true,treasureGame.validNumberOfPlayers(5));
     }
 
+    /**
+     * This method tests whether an exception is generated whenever a bad input for the number of players is inserted
+     * @throws InvalidNumberOfPlayersException
+     * Whenever a bad input is inserted , in this test this gives us a good test result.
+     */
     @Test
     public void testInvalidNumberOfPlayersInput() throws InvalidNumberOfPlayersException{
         exceptionExcepted.expect(InvalidNumberOfPlayersException.class);
         treasureGame.validNumberOfPlayers(9);
     }
 
+    /**
+     * This method tests that the validation for the map size is correct
+     * @throws InvalidMapSizeException
+     * Whenever the map size is not within range and thus the test fails due to an InvalidMapSizeException that was
+     * not expected
+     */
     @Test
     public void testValidMapSizeInput()throws InvalidMapSizeException{
         assertEquals(true,treasureGame.validMapSize(4,5));
@@ -44,12 +72,34 @@ public class GameEngineTest {
         assertEquals(true,treasureGame.validMapSize(5,25));
     }
 
+    /**
+     * This method tests whether an InvalidMapSizeException is generated when a bad input is outside the range , this
+     * tests when the number of players is less than or equal to 4
+     * @throws InvalidMapSizeException
+     * Whenever the map size is out of range , in this test case this yields a good test result
+     */
     @Test
-    public void testInvalidMapSizeInput()throws InvalidMapSizeException{
+    public void testInvalidMapSizeInput1()throws InvalidMapSizeException{
         exceptionExcepted.expect(InvalidMapSizeException.class);
-        assertEquals(true,treasureGame.validMapSize(4,4));
+        treasureGame.validMapSize(4,4);
     }
 
+    /**
+     * This method tests whether an InvalidMapSizeException is generated when a bad input is outside the range , this
+     * tests when the number of players is greater than 4
+     * @throws InvalidMapSizeException
+     * Whenever the map size is out of range , in this test case this yields a good test result
+     */
+    @Test
+    public void testInvalidMapSizeInput2()throws InvalidMapSizeException{
+        exceptionExcepted.expect(InvalidMapSizeException.class);
+        treasureGame.validMapSize(5,4);
+    }
+
+    /**
+     * This test is used in order to check that the validation for the starting position is correct , i.e that
+     * each starting position is on a grass tile.
+     */
     @Test
     public void testStartingPositionCorrectness(){
         treasureGame.map = new Map(5);
@@ -66,6 +116,11 @@ public class GameEngineTest {
         }
     }
 
+    /**
+     * This test is used to check that the initialize game players method initialized the player array correctly
+     * i.e that it is not null and that its length is greater than 0 , this shows that the initialize player method
+     * worked correctly
+     */
     @Test
     public void testPlayerArrayInitializedCorrectly(){
         treasureGame.map = new Map(5);
@@ -74,6 +129,11 @@ public class GameEngineTest {
         assertEquals(2,treasureGame.players.length);
     }
 
+    /**
+     * This test is used to check that the initialize game players method initialized the starting position array correctly
+     * i.e that it is not null and that its length is greater than 0 , this shows that the initialize player method
+     * worked correctly
+     */
     @Test
     public void testStartingPositionArrayInitializedCorrectly(){
         treasureGame.map = new Map(5);
@@ -82,8 +142,9 @@ public class GameEngineTest {
         assertEquals(2,treasureGame.startingPosition.length);
     }
 
-
-
+    /**
+     * This method makes removes the allocated memory to the object.
+     */
     @After
     public void tearDown(){
         treasureGame = null;
