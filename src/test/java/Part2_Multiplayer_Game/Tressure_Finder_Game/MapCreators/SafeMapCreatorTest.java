e0a1db22081ebaa40411b6e85ef89de964452edb
Gabriel Camilleri <gcamilleri1997@gmail.com>
2018-04-27 14:38:31 +0200

    Added comments in the tests of map creators

--- src/test/java/Part2_Multiplayer_Game/Tressure_Finder_Game/MapCreators/SafeMapCreatorTest.java
+++ src/test/java/Part2_Multiplayer_Game/Tressure_Finder_Game/MapCreators/SafeMapCreatorTest.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * This class is used to test the methods found in the class SafeMapCreator
+ */
 public class SafeMapCreatorTest {
 
+    //Both variables are being used for the tests
     private int mapSize1;
     private SafeMapCreator creator1;
 
@@ -20,9 +24,16 @@ public class SafeMapCreatorTest {
 
     @After
     public void tearDown() {
+        mapSize1 = 0;
+        creator1= null;
     }
 
     @Test
+    /**
+     * This test is used to check if when creating a map from this class,
+     * it is actually creating a map with the type of 'Safe' as well checking
+     * it has the inputted map size to ensure it was inserted in the right constructor
+     */
     public void testCreateSafeMap() {
         Map sM = creator1.createMap("Safe",mapSize1);
         int size =  sM.getMapSize();
