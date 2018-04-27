2cf10e7d5b60a6d6b02f2ea2361d433eb3198eed
Dylan Galea <dillu_galea@hotmail.com>
2018-04-27 14:27:05 +0200

    player maps

--- src/main/java/Part2_Multiplayer_Game/Tressure_Finder_Game/MapCreators/SafeMapCreator.java
+++ src/main/java/Part2_Multiplayer_Game/Tressure_Finder_Game/MapCreators/SafeMapCreator.java
@@ -4,9 +4,7 @@ import Part2_Multiplayer_Game.Tressure_Finder_Game.Map;
 import Part2_Multiplayer_Game.Tressure_Finder_Game.MapCreator;
 import Part2_Multiplayer_Game.Tressure_Finder_Game.Maps.SafeMap;
 
-/**
- * Created by Dylan Galea on 27/04/2018.
- */
+
 public class SafeMapCreator extends MapCreator {
     public Map createMap(String Type,int size){
         SafeMap map = new SafeMap(size);
